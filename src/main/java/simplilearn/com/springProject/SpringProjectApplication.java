a1285dda162b7a1ad530ae195c53f5ff91e9da46
Atul Kumbhar <atul.kumbhar1985@gmail.com>
2021-06-06 20:05:31 +0530

    spring porject 1

---
+++ src/main/java/simplilearn/com/springProject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package simplilearn.com.springProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
